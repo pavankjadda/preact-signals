4a4703d6e452de1af392bb08bc7beb7b636c9a09
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2025-06-08 23:03:00 -0400

    feat(FindBook): refactor search functionality to use signals for state management

--- src/features/book/find-book/FindBook.tsx
+++ src/features/book/find-book/FindBook.tsx
@@ -1,35 +1,40 @@
-import { Grid, InputAdornment, Paper, TextField } from '@mui/material';
-import React, { useState } from 'react';
+import {Grid, InputAdornment, Paper, TextField} from '@mui/material';
+import React from 'react';
 import FindBookTable from './FindBookTable';
 import SearchIcon from '@mui/icons-material/Search';
+import {signal, useSignal} from '@preact/signals-react';
 
+export const globalSearchText = signal('');
 export default function FindBook() {
-	const [searchText, setSearchText] = useState<string>('');
-	return (
-		<Grid container className="custom-flex-justify-center" style={{ height: 'auto', width: '100%' }}>
-			<Grid size={{ xs: 12, sm: 12, md: 12, lg: 11, xl: 11 }}>
-				<Paper elevation={24} style={{ marginTop: '30px' }}>
-					<h2 className={'custom-flex-justify-center'}>Find Book</h2>
-					<hr />
-					<div style={{ margin: '50px' }}>
-						<TextField
-							style={{ width: '100%' }}
-							id="search-text"
-							label="Find Book"
-							variant="filled"
-							onChange={(event: React.ChangeEvent<HTMLInputElement>) => setSearchText(event.target.value)}
-							InputProps={{
-								startAdornment: (
-									<InputAdornment position="start">
-										<SearchIcon />
-									</InputAdornment>
-								),
-							}}
-						/>
-					</div>
-					<FindBookTable searchText={searchText} />
-				</Paper>
-			</Grid>
-		</Grid>
-	);
+    const searchText = useSignal('');
+
+    return (
+        <Grid container className="custom-flex-justify-center" style={{height: 'auto', width: '100%'}}>
+            <Grid size={{xs: 12, sm: 12, md: 12, lg: 11, xl: 11}}>
+                <Paper elevation={24} style={{marginTop: '30px'}}>
+                    <h2 className={'custom-flex-justify-center'}>Find Book</h2>
+                    <hr/>
+                    <div style={{margin: '50px'}}>
+                        <TextField
+                            style={{width: '100%'}}
+                            id="search-text"
+                            label="Find Book"
+                            variant="filled"
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                searchText.value = event.target.value;
+                            }}
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <SearchIcon/>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                    </div>
+                    <FindBookTable searchText={searchText.value}/>
+                </Paper>
+            </Grid>
+        </Grid>
+    );
 }
--- src/features/book/find-book/FindBookTable.tsx
+++ src/features/book/find-book/FindBookTable.tsx
@@ -1,44 +1,46 @@
-import { DataGrid, GridCellParams, GridColDef } from '@mui/x-data-grid';
-import { useMemo } from 'react';
-import { Link } from 'react-router-dom';
-import { isUndefinedOrNullOrEmpty } from '../../../util/StringUtils';
-import { useQuery } from '@tanstack/react-query';
-import { BookService } from '../../../services/BookService';
+import {DataGrid, GridCellParams, GridColDef} from '@mui/x-data-grid';
+import {useMemo} from 'react';
+import {Link} from 'react-router-dom';
+import {isUndefinedOrNullOrEmpty} from '../../../util/StringUtils';
+import {useQuery} from '@tanstack/react-query';
+import {BookService} from '../../../services/BookService';
 
 export default function FindBookTable(props: { searchText: string }) {
-	const { data: books, isLoading } = useQuery({
-		queryKey: ['books'],
-		queryFn: () => BookService.getAllBooks(),
-	});
+    const {data: books, isLoading} = useQuery({
+        queryKey: ['books'],
+        queryFn: () => BookService.getAllBooks(),
+    });
 
-	const filteredBooks = useMemo(() => {
-		return isUndefinedOrNullOrEmpty(props.searchText)
-			? books
-			: books?.filter((row) => {
-					return Object.keys(row).some((field) => {
-						// @ts-ignore
-						return row[field] == null ? false : new RegExp(props.searchText, 'i').test(row[field].toString());
-					});
-			  });
-	}, [props.searchText, isLoading]);
+    console.log('FindBookTable:searchText:', props.searchText);
 
-	return (
-		<div style={{ display: 'flex', height: '100%' }}>
-			<div style={{ flexGrow: 1 }}>
-				<DataGrid
-					initialState={{ pagination: { paginationModel: { pageSize: 10 } } }}
-					getRowId={(row) => row.id}
-					pageSizeOptions={[10, 20, 50, 100]}
-					disableRowSelectionOnClick={true}
-					density={'comfortable'}
-					loading={isLoading}
-					rows={filteredBooks ?? []}
-					columns={columns}
-					autoHeight={true}
-				/>
-			</div>
-		</div>
-	);
+    const filteredBooks = useMemo(() => {
+        return isUndefinedOrNullOrEmpty(props.searchText)
+            ? books
+            : books?.filter((row) => {
+                return Object.keys(row).some((field) => {
+                    // @ts-ignore
+                    return row[field] == null ? false : new RegExp(props.searchText, 'i').test(row[field].toString());
+                });
+            });
+    }, [props.searchText, isLoading]);
+
+    return (
+        <div style={{display: 'flex', height: '100%'}}>
+            <div style={{flexGrow: 1}}>
+                <DataGrid
+                    initialState={{pagination: {paginationModel: {pageSize: 10}}}}
+                    getRowId={(row) => row.id}
+                    pageSizeOptions={[10, 20, 50, 100]}
+                    disableRowSelectionOnClick={true}
+                    density={'comfortable'}
+                    loading={isLoading}
+                    rows={filteredBooks ?? []}
+                    columns={columns}
+                    autoHeight={true}
+                />
+            </div>
+        </div>
+    );
 }
 
 /**
@@ -48,13 +50,13 @@ export default function FindBookTable(props: { searchText: string }) {
  * @since 0.1.0
  */
 const columns: GridColDef[] = [
-	{
-		field: 'id',
-		headerName: 'ID',
-		flex: 1,
-		renderCell: (params: GridCellParams) => <Link to={'/book/' + params.row['id']}>{params.row['id']}</Link>,
-	},
-	{ field: 'title', headerName: 'Title', flex: 2.5 },
-	{ field: 'isbn', headerName: 'ISBN', flex: 2.5 },
-	{ field: 'author', headerName: 'Author', flex: 2.5 },
+    {
+        field: 'id',
+        headerName: 'ID',
+        flex: 1,
+        renderCell: (params: GridCellParams) => <Link to={'/book/' + params.row['id']}>{params.row['id']}</Link>,
+    },
+    {field: 'title', headerName: 'Title', flex: 2.5},
+    {field: 'isbn', headerName: 'ISBN', flex: 2.5},
+    {field: 'author', headerName: 'Author', flex: 2.5},
 ];
